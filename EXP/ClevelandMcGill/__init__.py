cca677f547b987b7f07d5cfd14d0f370eefd7b52
Daniel Haehn <haehn@seas.harvard.edu>
2018-03-20 21:16:44 -0400

    Cleaned everything up!

--- EXP/ClevelandMcGill/__init__.py
+++ EXP/ClevelandMcGill/__init__.py
@@ -1,7 +1,3 @@
 from figure1 import Figure1
 from figure3 import Figure3
-
-from figure1_length import Figure1_Length
-from figure1_position_nonaligned_scale import Figure1_Position_Nonaligned_Scale
-from figure1_position_common_scale import Figure1_Position_Common_Scale
 from figure12 import Figure12
