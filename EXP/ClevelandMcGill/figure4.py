6e9f6ebd59b26b470c002cec36a99d92aa166c4f
Daniel Haehn <haehn@seas.harvard.edu>
2018-03-24 17:21:48 -0400

    Ready for the position-length experiment!

--- EXP/ClevelandMcGill/figure4.py
+++ EXP/ClevelandMcGill/figure4.py
@@ -180,7 +180,7 @@ class Figure4:
 
     all_values[5] = data[1] # fixed pos but max. 56
     current_max = 93-all_values[5]
-    print current_max
+    # print current_max
     all_values[6] = np.random.randint(8,current_max/4.)
 
     all_values[7] = np.random.randint(8,current_max/4.)
@@ -304,6 +304,88 @@ class Figure4:
         barchart[99-current+(int(d)/2):99-current+(int(d)/2)+1, 75:76] = 1
 
 
+
+
+    return barchart
+
+
+
+
+  @staticmethod
+  def data_to_type5(data):
+    '''
+    '''
+    barchart = np.zeros((100,100), dtype=np.bool)
+
+    # we build the barchart to the top
+    all_values = [0] * 10
+
+    current_max = 93-data[0]-data[1]
+
+    if current_max <= 9:
+      # this won't work
+      raise Exception('Out of bounds')
+
+    all_values[0] = np.random.randint(3,current_max/3.)
+    
+    all_values[1] = np.random.randint(3,current_max/3.)
+
+    all_values[2] = np.random.randint(3,current_max/3.)
+
+    all_values[3] = data[0]
+
+    all_values[4] = data[1]
+
+    current_max = np.sum(all_values[0:5])
+
+    # draw left, right of the left stacked barchart
+    rr, cc = skimage.draw.line(99, 10, 99-int(current_max), 10)
+    barchart[rr, cc] = 1
+
+    rr, cc = skimage.draw.line(99, 40, 99-int(current_max), 40)
+    barchart[rr, cc] = 1
+
+    current = 0
+    for i,d in enumerate(all_values):
+
+      rr, cc = skimage.draw.line(99-(int(d)+current), 10, 99-(int(d)+current), 40)
+      barchart[rr, cc] = 1
+      current += int(d)
+
+      if i == 3 or i ==4:
+        # mark the max
+        # print current, d
+        barchart[99-current+(int(d)/2):99-current+(int(d)/2)+1, 25:26] = 1
+
+
+
+    current_max = 93
+
+    all_values[5] = np.random.randint(8,current_max/5.)
+    
+    all_values[6] = np.random.randint(8,current_max/5.)
+
+    all_values[7] = np.random.randint(8,current_max/5.)
+
+    all_values[8] = np.random.randint(8,current_max/5.)
+
+    all_values[9] = np.random.randint(8,current_max/5.)
+
+    current_max = np.sum(all_values[5:])
+
+    # draw left, right of the left stacked barchart
+    rr, cc = skimage.draw.line(99, 60, 99-int(current_max), 60)
+    barchart[rr, cc] = 1
+
+    rr, cc = skimage.draw.line(99, 90, 99-int(current_max), 90)
+    barchart[rr, cc] = 1
+
+    current = 0
+    for i,d in enumerate(all_values[5:]):
+
+      rr, cc = skimage.draw.line(99-(int(d)+current), 60, 99-(int(d)+current), 90)
+      barchart[rr, cc] = 1
+      current += int(d)
 
 
     return barchart
